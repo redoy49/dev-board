6b2544cc7c6dbad8c79c0437cb122b8100160195
Redoy Al Hasan <mdredoyhasan49@gmail.com>
2025-03-01 23:45:34 +0600

    Third commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log(5);
+const completedBtns = document.querySelectorAll("#completed-btn");
+
+const taskNumber = document.getElementById("task-number");
+const doneNumber = document.getElementById("done-number");
+
+const changeText = document.getElementById("change-text");
+
+const title = document.getElementById("title").innerText;
+
+for (const completedBtn of completedBtns) {
+  completedBtn.addEventListener("click", function (event) {
+    alert("Board Update Successfully");
+    event.target.disabled = true;
+
+    let convertTaskNumber = parseInt(taskNumber.innerText);
+    let convertDoneNumber = parseInt(doneNumber.innerText);
+
+    taskNumber.innerText = convertTaskNumber - 1;
+    doneNumber.innerText = convertDoneNumber + 1;
+    
+    changeText.innerText = `You have Complete The Task ${title} at 12:48:15 PM`;
+  });
+}
---
+++ task.md
@@ -0,0 +1,3 @@
+1 dynamic date
+2. completed button click korle ekta alert diye dibe 
+3. 6 lekha 1 kore kombe , 23 1 kore varbe
