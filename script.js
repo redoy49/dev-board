41e4e973066df48d3374d04f2e448b7821a34066
Redoy Al Hasan <mdredoyhasan49@gmail.com>
2025-03-02 15:22:07 +0600

    done

--- script.js
+++ script.js
@@ -27,7 +27,10 @@ for (const completedBtn of completedBtns) {
     const buttonContainer = event.target.parentElement;
     const buttonMainContainer = buttonContainer.parentElement;
     const title = buttonMainContainer.querySelector("h2").innerText;
-    p.innerText = `You have Complete The Task ${title} at 12:48:15 PM`;
+
+    const todayDateResult = todayDateFun();
+
+    p.innerText = `You have Complete The Task ${title} at ${todayDateResult.todayTime} PM`;
   });
 }
 
@@ -37,3 +40,34 @@ document
   .addEventListener("click", function () {
     activityContainer.innerHTML = "";
   });
+
+// Discover something
+document.getElementById("discover").addEventListener("click", function () {
+  window.location.href = "page/blog.html";
+});
+
+function todayDateFun() {
+  const date = new Date();
+  const todayDate = date.toDateString(); // "Sun Mar 02 2025"
+  const todayTime = date.toTimeString().split(" ")[0]; // "11:22:29"
+  return { todayDate, todayTime };
+}
+
+const todayDateShow = todayDateFun();
+
+console.log(todayDateShow.todayTime);
+
+const ajkerDate = document.getElementById("ajker-date");
+ajkerDate.innerText = todayDateShow.todayDate;
+console.log(ajkerDate.innerText);
+
+// Change Background Color
+document.getElementById("change-color").addEventListener("click", function () {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  const changeColor = `rgb(${red}, ${green}, ${blue})`;
+  document.body.style.backgroundColor = changeColor;
+  console.log(changeColor);
+});
---
+++ tect.js
@@ -0,0 +1,6 @@
+const date = new Date();
+const realTime = date.toLocaleTimeString;
+const realDate = date.toLocaleDateString;
+
+console.log(realTime);
+console.log(realDate.innerText);
