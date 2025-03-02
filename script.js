52a6f115a9aede0e8b36514dd4ec1ce81d0a18c8
Redoy Al Hasan <mdredoyhasan49@gmail.com>
2025-03-02 09:50:03 +0600

    Fourth commit

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const changeText = document.getElementById("change-text");
 
 const title = document.getElementById("title").innerText;
 
+const activityContainer = document.getElementById("activity-container");
+
 for (const completedBtn of completedBtns) {
   completedBtn.addEventListener("click", function (event) {
     alert("Board Update Successfully");
@@ -17,7 +19,21 @@ for (const completedBtn of completedBtns) {
 
     taskNumber.innerText = convertTaskNumber - 1;
     doneNumber.innerText = convertDoneNumber + 1;
-    
-    changeText.innerText = `You have Complete The Task ${title} at 12:48:15 PM`;
+
+    const p = document.createElement("p");
+    p.classList.add("activity-log");
+    activityContainer.appendChild(p);
+
+    const buttonContainer = event.target.parentElement;
+    const buttonMainContainer = buttonContainer.parentElement;
+    const title = buttonMainContainer.querySelector("h2").innerText;
+    p.innerText = `You have Complete The Task ${title} at 12:48:15 PM`;
   });
 }
+
+// Activity Log
+document
+  .getElementById("clear-history-btn")
+  .addEventListener("click", function () {
+    activityContainer.innerHTML = "";
+  });
--- task.md
+++ task.md
@@ -1,3 +1,4 @@
 1 dynamic date
 2. completed button click korle ekta alert diye dibe 
 3. 6 lekha 1 kore kombe , 23 1 kore varbe
+clear a click korle sob clear hoye jabe
